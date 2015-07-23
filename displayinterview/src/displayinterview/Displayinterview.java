b91540dcab273ac45ca9f7a4b4b03839ba7c3f3f
Ikram_Lim <ikramlim@gmail.com>
2015-07-23 22:33:29 +0800

    forloop
    1/5 = 0.2
    2/5 = 0.4
    3/5 = 0.6
    4/5 = 0.8
    5/5 = 1.0
    ------------

---
+++ displayinterview/src/displayinterview/Displayinterview.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package displayinterview;
+
+/**
+ *
+ * @author ikramlim
+ */
+
+public class Displayinterview {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        int i = 1;
+        int j = 6;
+        
+       for (i = 1 ; i <= 5; i++)
+       {
+                System.out.println ( i + "/5 = " + i /5.0);
+       }
+       
+        System.out.println("--------------------");
+       
+    
+            for (j = 6 ; j <= 10; j++)
+            {
+                    System.out.println ( j + "/5 = " + j /5.0);
+            }
+        System.out.println("====================");
+    }
+}
